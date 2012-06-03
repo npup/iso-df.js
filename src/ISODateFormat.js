38b9eb223a3930dbee1cb393aa9604197c0f98c8
P. Envall <petter.envall@gmail.com>
2012-06-03 23:09:17 +0200

    Implement format of dates

--- src/ISODateFormat.js
+++ src/ISODateFormat.js
@@ -4,10 +4,24 @@ var ISODateFormat = (function () {
 		(typeof type=="undefined") && (type = Type.date);
 		this.type = type;
 	}
+	
+	function format(date) {
+		var yyyy = date.getFullYear()
+			, MM = date.getMonth()+1
+			, dd = date.getDate()
+			, str;
+		var yearString = ""+yyyy;
+		while (yearString.length<4) {
+			yearString = "0"+yearString;
+		}
+		str = ([yearString, (MM<10?("0"+MM):MM), (dd<10?("0"+dd):dd)].join("-"));
+		return str;
+	}
+	
 	Formatter.prototype = {
 		"constructor": Formatter
 		, "parse": function () {}
-		, "format": function () {}
+		, "format": format
 	};
 	
 	var Type = {
